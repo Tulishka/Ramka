b4c29f759d88678ccf5751e615fe96dccc50a8c8
Ivan Priymenko <i.priymenko@saticogroup.com>
2022-08-10 14:35:19 +0700

    snake exaple

--- ramka/layers.py
+++ ramka/layers.py
@@ -20,15 +20,25 @@ class Layer:
         if game_object in self.gameObjects:
             self.gameObjects.remove(game_object)
 
+    def change_order(self, object: GameObject, delta=None, order=None):
 
-    def change_order(self, object: GameObject, delta):
-        idx=self.gameObjects.index(object)
-        nidx=idx+delta
-        if nidx<0 or nidx>=len(self.gameObjects):
+        if delta is not None:
+            idx = self.gameObjects.index(object)
+            nidx = idx + delta
+        else:
+            nidx = order
+
+        if nidx < 0 or nidx >= len(self.gameObjects):
             return
 
         self.gameObjects.remove(object)
-        if nidx>idx and nidx>0:
-            nidx-=1
+        if nidx > idx and nidx > 0:
+            nidx -= 1
 
-        self.gameObjects.insert(nidx,object)
+        self.gameObjects.insert(nidx, object)
+
+    def change_order_first(self, object: GameObject):
+        self.change_order(object, order=0)
+
+    def change_order_last(self, object: GameObject):
+        self.change_order(object, order=len(self.gameObjects) - 1)

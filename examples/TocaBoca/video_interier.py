cc8c14292ae814c31d53de94529543a6a01912f9
Ivan Priymenko <i.priymenko@saticogroup.com>
2022-09-01 20:48:30 +0700

    snake exaple

--- examples/TocaBoca/video_interier.py
+++ examples/TocaBoca/video_interier.py
@@ -1,6 +1,8 @@
 from typing import Tuple
 
 from interier import Interier
+from ramka import Game, Vector, interp_func_cubic
+from ramka.gameobject_animators import ScaleAnimator
 from ramka.video_sprite import VideoSprite
 
 
@@ -8,16 +10,27 @@ class VideoInterier(Interier):
     def __init__(self, anim, pos, video_file: Tuple[str, Tuple[int, int]], video_pos, **kwargs):
         super().__init__(anim, pos, **kwargs)
 
-        video_sprite = VideoSprite(video_file[0], size=video_file[1])
-        video_sprite.pos(video_pos).transform.set_parent(self)
-
+        self.video_sprite = None
         self.video_file = video_file
         self.video_pos = video_pos
+        # self.create_video_object(False)
+
+    def create_video_object(self, add_to_game):
+        self.video_sprite = VideoSprite(self.video_file[0], size=self.video_file[1])
+        self.video_sprite.pos(self.video_pos).transform.set_parent(self)
+
+        if add_to_game:
+            Game.add_object(self.video_sprite)
+
+    def remove_video_object(self):
+        if self.video_sprite:
+            Game.remove_object(self.video_sprite)
+            self.video_sprite = None
 
     @staticmethod
     def get_creation_params(dict, parent):
-        p=super(VideoInterier,VideoInterier).get_creation_params(dict,parent)
-        d=p[1]
+        p = super(VideoInterier, VideoInterier).get_creation_params(dict, parent)
+        d = p[1]
         d.update({
             "video_file": dict["video_file"],
             "video_pos": dict["video_pos"],
@@ -29,6 +42,21 @@ class VideoInterier(Interier):
         res.update({
             "video_file": self.video_file,
             "video_pos": self.video_pos,
+            "video_enabled": bool(self.video_sprite),
         })
         return res
 
+    def init_from_dict(self, opts):
+        super().init_from_dict(opts)
+        if opts.get("video_enabled", False):
+            self.create_video_object(False)
+
+    @Game.on_mouse_up(button=1)
+    def on_mouse_up(self):
+        if self.mouse_start_point:
+            if self.video_sprite:
+                ScaleAnimator(self.video_sprite, Vector(1, 0.02), 0.15, interp_func=interp_func_cubic)().do(lambda pi: self.remove_video_object()).kill()
+            else:
+                self.create_video_object(True)
+                self.video_sprite.transform.scale=1,0.02
+                ScaleAnimator(self.video_sprite, Vector(1, 1), 0.15, interp_func=interp_func_cubic)().kill()

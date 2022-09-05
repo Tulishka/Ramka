5e7ae1d65b54c0ce5b1910582e4b45c8f371399f
Ivan Priymenko <i.priymenko@saticogroup.com>
2022-09-05 22:47:08 +0700

    snake exaple

--- ramka/gameobject.py
+++ ramka/gameobject.py
@@ -80,7 +80,7 @@ class GameObject:
             if (clas is None or isinstance(c.gameObject, clas)) and (not callable(filter) or filter(c.gameObject)):
                 yield c.gameObject
             if recursive:
-                for v in c.gameObject.get_children(True):
+                for v in c.gameObject.get_children(recursive=recursive, clas=clas, filter=filter):
                     yield v
 
     def get_parent(self, clas=None, filter: Callable[[GameObject], bool] = None) -> GameObject:

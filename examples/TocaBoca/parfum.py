b547187d2a0d169fedf3807f413d93644776db01
ira <ira@mail.ru>
2022-09-10 14:19:49 +0700

    g

--- examples/TocaBoca/parfum.py
+++ examples/TocaBoca/parfum.py
@@ -1,9 +1,9 @@
 import math
-from random import random, randint
+from random import random, randint, choice
 
 from base_item_components import TurnToMoveDirection
 from item import Item
-from ramka import Game, Sprite, Vector, Camera
+from ramka import Game, Sprite, Vector, Camera, Animation
 from ramka.gameobject_animators import ScaleAnimator
 from ramka.object_generator import ObjectGenerator
 
@@ -23,24 +23,32 @@ class Oblachko(Sprite):
     max_size = 2
     angle = 45
 
+    anim = []
+
     def __init__(self, dir):
-        super().__init__("img/Particles/puz.png")
+        if not Oblachko.anim:
+            a1=Animation("img/Particles/oblachko1.png", 5, True)
+            a2=Animation("img/Particles/oblachko2.png", 5, True)
+            Oblachko.anim=[a1,a1,a1,a2]
+
+        super().__init__(choice(Oblachko.anim))
         r = random() * Oblachko.max_size * 0.5 + Oblachko.max_size * 0.5
         self.transform.scale = 0.3, 0.3
         self.spd = Vector(dir, 0).rotate(randint(-Oblachko.angle, Oblachko.angle))
-        self.lspd=randint(600,800)
+        self.lspd = randint(600, 800)
         self.life_time = 1 + random() * 0.7
         ScaleAnimator(self, r, self.life_time, interp_func=size_interp)().kill()
 
     def update(self, deltaTime: float):
         super().update(deltaTime)
 
-        step = self.spd * deltaTime * spd_interp(self.time / self.life_time) * self.lspd
+        aspd= deltaTime * spd_interp(self.time / self.life_time) * self.lspd
+        step = self.spd * aspd
 
         self.transform.pos += step
+        self.transform.angle+=max(6*aspd,50*deltaTime)
 
-        if self.time >= self.life_time-0.1:
-
+        if self.time >= self.life_time - 0.1:
             Game.remove_object(self)
 
 
@@ -52,9 +60,9 @@ class Parfum(Item):
 
     @Game.on_mouse_down(button=3)
     def pshik(self):
-        p = ObjectGenerator(self.create_particle, 0.7, [14,7,3], loop_callback=lambda x: Game.remove_object(x))
+        p = ObjectGenerator(self.create_particle, 0.7, [14, 7, 3], loop_callback=lambda x: Game.remove_object(x))
         Game.add_object(p)
 
     def create_particle(self, c):
-        o = Oblachko(1 if self.transform.scale_x < 0 else -1).pos(self.transform.pos+self.get_pretty_point("soplo"))
+        o = Oblachko(1 if self.transform.scale_x < 0 else -1).pos(self.transform.pos + self.get_pretty_point("soplo"))
         return o

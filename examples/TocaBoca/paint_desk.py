5d6655564d653554a0f6d07b1203410ac026f986
Ivan Priymenko <i.priymenko@saticogroup.com>
2022-09-11 17:04:38 +0700

    snake exaple

--- examples/TocaBoca/paint_desk.py
+++ examples/TocaBoca/paint_desk.py
@@ -1,40 +1,55 @@
-from random import randint
+from random import randint, random
 
 import pygame
 
+from examples.Components.DragAndDrop import DragAndDropController
 from interier import Interier
 from ramka import Vector, Game, Input
 
 
 class PaintDesk(Interier):
-
-    size=(420,548)
-    size2=Vector(size)*0.5
+    size = (420, 548)
+    size2 = Vector(size) * 0.5 - Vector(1,1)
 
     def __init__(self, *a, **b):
         super().__init__(*a, **b)
         self.surface = pygame.Surface(PaintDesk.size)
         self.clear()
-        self.spread=10
+        self.spread = 10
+        self.color = (255, 10, 100)
+        self.last_draw_point = None
 
     def clear(self):
-        self.surface.fill((255,255,255))
+        self.surface.fill((255, 255, 255))
 
-    @Game.on_mouse_down(button=1,continuos=True)
+    @Game.on_mouse_up(button=1, hover=False)
+    def end_paint(self):
+        self.last_draw_point = None
+
+    @Game.on_mouse_down(button=1, continuos=True)
     def paint(self):
-        dt=Game.deltaTime()
-        p=self.transform.to_local_coord(self.transform,Input.mouse_pos,False)+PaintDesk.size2
+        if pygame.key.get_mods() & pygame.KMOD_LSHIFT or DragAndDropController.controller.get_dragged_object():
+            return False
 
-        for i in range(int(1000*dt)):
-            self.surface.set_at((int(p[0])+randint(-self.spread,self.spread),int(p[1])+randint(-self.spread,self.spread)),(255,0,0))
+        dt = Game.deltaTime()
+        p = self.transform.to_local_coord(self.transform, Input.mouse_pos, False) + PaintDesk.size2
 
-        Game.break_event_loop=True
+        if self.last_draw_point:
+            pygame.draw.line(self.surface, self.color, self.last_draw_point, p, int(self.spread * 2.2))
+        pygame.draw.circle(self.surface, self.color, p, self.spread)
+        # v = Vector(self.spread * (1 + random() * 2), 0)
+        # for i in range(int(1000 * dt)):
+        #     v.rotate_ip(randint(0, 270))
+        # self.surface.set_at((int(p[0] + v.x), int(p[1] + v.y)), (255, 10, 200))
+
+        self.last_draw_point = p
+
+        Game.break_event_loop = True
         return self
 
     def draw(self, dest: pygame.Surface):
         super().draw(dest)
-        p=self.screen_pos()-PaintDesk.size2
-        dest.blit(self.surface,p)
+        p = self.screen_pos() - PaintDesk.size2
+        dest.blit(self.surface, p)
         if self.touch_test(Input.mouse_pos):
-            pygame.draw.circle(dest,(0,255,255),Input.mouse_pos,self.spread,1)
-
+            pygame.draw.circle(dest, (0, 255, 255), Input.mouse_pos, self.spread, 1)
